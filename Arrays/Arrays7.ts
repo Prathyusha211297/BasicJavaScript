623ded709a6d1b63aed46c32c5c0cbcf3d2f418e
Prathyusha <prathyusha.h212@gmail.com>
2026-06-08 16:13:57 +0530

    updating all programs

---
+++ Arrays/Arrays7.ts
@@ -0,0 +1,13 @@
+let animals: string[]=["dog","cat","tiger","lion","elephant"];
+
+animals.unshift("monkey");
+console.log(animals);
+
+animals.shift();
+console.log(animals);
+
+animals.push("giraffe");
+console.log(animals);
+
+animals.pop();
+console.log(animals);
---
+++ swappingusingassignment.ts
@@ -0,0 +1,10 @@
+let a: number=10; 
+let b: number=20;
+let c:number = a;    //c=10;
+a=b;							//a=20
+b=c;							//b=10
+
+console.log("a value is" + a);
+console.log ("b value is" +b);
+
+
